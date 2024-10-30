904401d25bcf180a8e7af37da6cd09cbde46f7da
LU1DY <152814333+LU1DY@users.noreply.github.com>
2024-10-30 15:16:15 -0300

    Update adm.py
    comentários att

--- app/adm.py
+++ app/adm.py
@@ -2,8 +2,8 @@ from app.models import database, Usuario
 from app import app, bcrypt
 
 with app.app_context():
-    database.create_all()  #cria as tabelas
-# Cria um usuário administrador com base nos dados
+    # database.create_all()  #cria as tabelas
+    # Cria um usuário administrador com base nos dados
     admin_user = Usuario(username="ADMluidy1", email="luidy@gmail.com", senha=bcrypt.generate_password_hash("5002023Luidy").decode('utf-8'), is_admin=True)
     database.session.add(admin_user)
     database.session.commit()
