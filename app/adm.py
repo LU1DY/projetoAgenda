9f0f5eca508a283f7686438575622a44c920e351
Luidy-Michael <luidymichael8@gmail.com>
2025-02-25 22:31:55 -0300

    responsividade

--- app/adm.py
+++ app/adm.py
@@ -1,10 +1,10 @@
-from app.models import database, Usuario
-from app import app, bcrypt
+from app.models import Usuario
+from app import app, bcrypt, database
 
 with app.app_context():
     database.create_all()  #cria as tabelas
     # Cria um usuário administrador com base nos dados necessários especificados no banco de dados
-    admin_user = Usuario(username="ADM1", email="ADM1@gmail.com", senha=bcrypt.generate_password_hash("amdadmadm").decode('utf-8'), is_admin=True)
+    admin_user = Usuario(username="adm", email="adm@gmail.com", senha=bcrypt.generate_password_hash("admadmadm").decode('utf-8'), is_admin=True)
     
     database.session.add(admin_user)
     database.session.commit()
